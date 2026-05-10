29bc8cda1336ce66e6dbd48fc040df77bcec1746
Angel <neq17320251@gmail.com>
2026-05-10 23:47:15 +0800

    基于sessiond登录

--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -12,14 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
-/**
- * <p>
- * 前端控制器
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
 @Slf4j
 @RestController
 @RequestMapping("/user")
@@ -36,8 +28,7 @@ public class UserController {
      */
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
-        // TODO 发送短信验证码并保存验证码
-        return Result.fail("功能未完成");
+        return userService.sendCode(phone, session);
     }
 
     /**
@@ -47,8 +38,7 @@ public class UserController {
      */
     @PostMapping("/login")
     public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session) {
-        // TODO 实现登录功能
-        return Result.fail("功能未完成");
+        return userService.login(loginForm, session);
     }
 
     /**
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,20 +1,74 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- * 服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
+import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
+
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1. 验证手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号不对哦");
+        }
+
+        // 2. 生成验证码 并保存在session里面
+        String code = RandomUtil.randomNumbers(6);
+        session.setAttribute("code", code);
+
+        // 3. 发送验证码到第三方平台
+        log.debug("你的验证码为:{}", code);
+
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 1. 取出验证码和手机号
+        String code = loginForm.getCode();
+        String phone = loginForm.getPhone();
+
+        // 2. 验证会话的手机号和验证码
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号不对哦");
+        }
+        Object cacheCode = session.getAttribute("code");
+        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+            return Result.fail("验证码不对哦");
+        }
+
+        // 3.根据手机号查用户是否存在
+        User user = query().eq("phone", phone).one();
+        if (user == null) {
+            // 4. 插入这个用户
+            user = createUserWithPhone(phone);
+
+        }
+
+        // 5. 保存到session中
+        session.setAttribute("user", user);
+        return Result.ok("登录成功");
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
 }
